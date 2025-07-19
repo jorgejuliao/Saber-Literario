0e62bf8705b817ed0774f632163934258c9a1edd
Jorge Luiz <165864220+jorgejuliao@users.noreply.github.com>
2025-07-19 14:48:28 -0300

    Delete saber-literário directory
    Vai ser deletado para colocar outro repositório atualizado.

--- saber-literário/script/catalogo-script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('limparFiltros').addEventListener('click', function() {
-    document.getElementById('categoria').value = '';
-    document.getElementById('subcategoria').value = '';
-    document.getElementById('autor').value = '';
-    document.getElementById('idioma').value = '';
-    document.getElementById('editora').value = '';
-});
